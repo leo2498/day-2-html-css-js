a94a8cb1e4512b9e8667551515d9ab2f4ecbf82b
Leo Juarez <juarezleo9@gmail.com>
2019-01-08 20:58:13 -0800

    added new questions

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 'use strict';
 
-  var doI = prompt('Do I have any allergies? Please answer with Y or N').toLowerCase();
-  console.log('Do I have any allergies ', doI);
+  var allergies = prompt('Do I have any allergies? Please answer with Y or N').toLowerCase();
+  console.log('Do I have any allergies? ', allergies);
 
-  if (doI === 'y' || doI === 'yes') {
+  if (allergies === 'y' || allergies === 'yes') {
     alert('Correct! I\'m allergic to dust.');
-  } else if (doI === 'n'|| doI ==='no') {
+  } else if (allergies === 'n'|| allergies ==='no') {
     alert('Wrong I do have one allergy.');
   } else { 
-    alert('Please answer with Y or N')
+    alert('Please answer with Y or N');
   }
+
+  var brokenBone = prompt('Have I ever broken any bones? Please answer with Y or N.').toLowerCase();
+  console.log('Have I broken a bone?', brokenBone);
+
+  if (brokenBone === 'y' || brokenBone === 'yes') {
+    alert('You are correct! I\'ve broken multiple bones.');
+  } else if (brokenBone === 'n' || brokenBone === 'no') {
+    alert('Sadly this you are wrong, I\'ve broken multiple.');
+  } else {
+    alert('Please answer with Y or N');
+  }
+
+  var sisters = prompt('Do I have any sisters? Please answer with Y or N.').toLowerCase();
+  console.log('Do I have sisters?', sisters);
+
+  if (sisters === 'y' || sisters === 'yes') {
+    alert('You are correct! I have 1 older sister.');
+  } else if (sisters === 'n' || sisters === 'no') {
+    alert('Incorrect.');
+  } else {
+    alert('Please answer with Y or N');
+  }
+
+  
